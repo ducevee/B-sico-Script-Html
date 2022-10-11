0237b9a9d6682813f930af2b3b0c024e4aa3925f
eduardo.silva <eduardo.silva@compwire.com.br>
2022-10-11 17:54:35 -0300

    adicionando mais um exercício de consultaar velo

---
+++ aula11/ex009.js
@@ -0,0 +1,9 @@
+var vel = 69.9
+console.log(`A velocidade do seu carro na via foi de ${vel}`)
+if (vel>70.0){
+    console.log(`Você ultrapassou a velocidade permitida, foi Multado!`)
+}
+else{
+    console.log(`A Velocidade do seu carro estava de acordo com a via. Continue dirigindo com segurança! ;)`)
+}
+console.log(`Dirija com cinto de segurança!`)
